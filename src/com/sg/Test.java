34b3b80dc29b256f85044b901da8f69d4ee50209
dmusicant <david.musicant@spiralgeneration.com>
2016-04-03 14:41:11 -0400

    Initial commit

---
+++ src/com/sg/Test.java
@@ -0,0 +1,15 @@
+package com.sg;
+
+/**
+ * Created by DMusicant on 4/3/16.
+ */
+public class Test {
+    private static int value = 0;
+
+    public static void main(String[] args)
+    {
+        Test.value = 1;
+
+        System.out.println( "First test of commit to GitHub");
+    }
+}
